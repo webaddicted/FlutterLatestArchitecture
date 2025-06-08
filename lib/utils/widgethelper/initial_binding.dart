e68025581456bc966c502ea5074d8412bd32a74a
mac <MicroPr1@123456789>
2025-06-08 21:05:17 +0530

    web

--- lib/utils/widgethelper/initial_binding.dart
+++ lib/utils/widgethelper/initial_binding.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:pingmexx/controller/login_controller.dart';
+import 'package:pingmexx/controller/theme_controller.dart';
 import 'package:pingmexx/data/repo/login_repo.dart';
 
 class InitialBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => LoginController(LoginRepo()), fenix: true);
+    Get.lazyPut(() => ThemeController(), fenix: true);
   }
 }
